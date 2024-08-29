3cbf6cf223963131b0a07b6c91c39179367dbed8
Tayrific <103253195+Tayrific@users.noreply.github.com>
2024-08-29 19:39:56 +0100

    outputs images with the promt

--- r.py
+++ r.py
@@ -8,7 +8,7 @@ load_dotenv()
 # Instantiate the OpenAI client with the API key from the environment variable
 client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
 
-def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0):
+def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=1):
     completion = client.chat.completions.create(
         model=model,
         temperature=temperature,
@@ -16,15 +16,41 @@ def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0)
     )
     return completion.choices[0].message.content
 
+def get_image(prompt):
+    image = client.images.generate(
+        model="dall-e-3",
+        prompt = prompt,
+        size = "1024x1024",
+        quality = "standard",
+        n=1
+    )
+    return image.data[0].url
+
+def extract_title(content):
+    lines = content.split('\n')
+    title = lines[0].strip() if lines else "default title"
+    return title
+
+
+
 messages=[
       {"role": "system", "content": "You are an Instagram content creator who likes anime. \
-                                       You will generate 1 recipe from a popular anime with emojis that are less than 500 characters long."},
-      {"role": "user", "content": "I want to create content on easy balanced diets."}
+                                       You will generate 1 recipe from a popular anime with emojis that are less than 900 characters long from different anime. the first line must be a title (do not label it with title) with the recipe and anime name"},
+      {"role": "user", "content": "I want to create content on healthy diets."}
 
   ]
-temperatures = [0, 0.5, 0.8, 1]
+temperatures = [1, 1, 1, 1]
 
 for temperature in temperatures:
     print(f"Temperature: {temperature}")
     content = get_completion_from_messages(messages=messages, temperature=temperature)
     print(content)
+
+    #Getting the title
+    title = extract_title(content)
+    print("Title:", title)
+
+    # Create an image based on the title
+    prompt = f"{title}"
+    image = get_image(prompt)
+    print("Image URL:", image)
